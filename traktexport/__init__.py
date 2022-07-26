c9cf7b5fd3d11a15db01741f6010522ea89276cf
Ahmedazim7804 <Ahmedazim7804@gmail.com>
2022-07-26 16:10:23 +0530

    windows path compatibility improved

--- traktexport/__init__.py
+++ traktexport/__init__.py
@@ -5,15 +5,13 @@ import trakt.core  # type: ignore[import]
 # trakt is configured by modifying module-level variables
 # set those up here, is setup before any other imports
 
-operating_system = platform.system()
-home_path = path.expanduser('~')
-if operating_system in ("Linux", "Darwin"):
-    data_dir = environ.get("XDG_DATA_HOME", path.join(home_path, ".local", "share"))
-elif operating_system == "Windows":
-    data_dir = path.join(home_path, '.traktexport')
-    makedirs(data_dir, exist_ok=True)
+if "XDG_DATA_HOME" in environ:
+   data_dir = environ["XDG_DATA_HOME"]
+elif platform.system() == "Windows":
+   data_dir = path.expanduser("~/.traktexport")
 else:
-    data_dir = home_path
+   data_dir = path.expanduser("~/.local/share")
+makedirs(data_dir, exist_ok=True)
 default_cfg_path = path.join(data_dir, "traktexport.json")
 traktexport_cfg = environ.get("TRAKTEXPORT_CFG", default_cfg_path)
 trakt.core.AUTH_METHOD = trakt.core.OAUTH_AUTH
