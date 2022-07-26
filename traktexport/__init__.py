fd9398093bc3ad1009da8d7b9c8d8c85a4b976ce
Sean Breckenridge <seanbrecke@gmail.com>
2022-07-26 03:57:21 -0700

    v0.1.4: support windows paths

--- traktexport/__init__.py
+++ traktexport/__init__.py
@@ -6,11 +6,11 @@ import trakt.core  # type: ignore[import]
 # set those up here, is setup before any other imports
 
 if "XDG_DATA_HOME" in environ:
-   data_dir = environ["XDG_DATA_HOME"]
+    data_dir = environ["XDG_DATA_HOME"]
 elif platform.system() == "Windows":
-   data_dir = path.expanduser("~/.traktexport")
+    data_dir = path.expanduser("~/.traktexport")
 else:
-   data_dir = path.expanduser("~/.local/share")
+    data_dir = path.expanduser("~/.local/share")
 makedirs(data_dir, exist_ok=True)
 default_cfg_path = path.join(data_dir, "traktexport.json")
 traktexport_cfg = environ.get("TRAKTEXPORT_CFG", default_cfg_path)
