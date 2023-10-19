f73197cf208518a10d0133cb93505ed1653f4439
Sean Breckenridge <seanbrecke@gmail.com>
2023-10-19 00:07:28 -0700

    add trakt-cli script

--- traktexport/export.py
+++ traktexport/export.py
@@ -1,6 +1,7 @@
 import os
+import logging
 from time import sleep
-from typing import Dict, Any, Iterator, List, Optional
+from typing import Dict, Any, Iterator, List, Optional, Literal
 from functools import lru_cache
 from urllib.parse import urljoin
 
@@ -27,7 +28,13 @@ SLEEP_TIME = int(os.environ.get("TRAKTEXPORT_SLEEP_TIME", 2))
 
 
 @backoff.on_exception(backoff.expo, (RateLimitException,))
-def _trakt_request(endpoint: str, data: Any = None) -> Any:
+def _trakt_request(
+    endpoint: str,
+    data: Any = None,
+    *,
+    logger: Optional[logging.Logger] = None,
+    method: Literal["get", "post", "patch"] = "get",
+) -> Any:
     """
     Uses CORE._handle_request (configured trakt session handled by trakt)
     to request information from Trakt
@@ -43,21 +50,28 @@ def _trakt_request(endpoint: str, data: Any = None) -> Any:
     """
     _check_config()
     url = urljoin(BASE_URL, endpoint)
-    logger.debug(f"Requesting '{url}'...")
-    json_data = CORE._handle_request(method="get", url=url, data=data)
+    if logger:
+        logger.debug(f"Requesting '{url}'...")
+    json_data = CORE._handle_request(method=method, url=url, data=data)
     sleep(SLEEP_TIME)
     return json_data
 
 
 def _trakt_paginate(
-    endpoint_bare: str, limit: int = 100, request_pages: Optional[int] = None
+    endpoint_bare: str,
+    limit: int = 100,
+    request_pages: Optional[int] = None,
+    logger: Optional[logging.Logger] = None,
 ) -> Iterator[Any]:
     page = 1
     while True:
-        items: List[Any] = _trakt_request(f"{endpoint_bare}?limit={limit}&page={page}")
+        items: List[Any] = _trakt_request(
+            f"{endpoint_bare}?limit={limit}&page={page}", logger=logger
+        )
         if len(items) == 0:
             break
-        logger.debug(f"First item: {items[0]}")
+        if logger:
+            logger.debug(f"First item: {items[0]}")
         yield from items
         page += 1
         if request_pages is not None and page > request_pages:
@@ -69,22 +83,26 @@ def full_export(username: str) -> Dict[str, Any]:
     return {
         "type": "full",
         "username": username,
-        "followers": _trakt_request(f"users/{username}/followers"),
-        "following": _trakt_request(f"users/{username}/following"),
-        "settings": _trakt_request("users/settings"),
-        "likes": _trakt_request("users/likes"),
-        "profile": _trakt_request(f"users/{username}"),
-        "comments": _trakt_request(f"users/{username}/comments"),
-        "lists": _trakt_request(f"users/{username}/lists"),
-        "ratings": _trakt_request(f"users/{username}/ratings"),
-        "recommendations": _trakt_request(f"users/{username}/recommendations"),
-        "watchlist": _trakt_request(f"users/{username}/watchlist"),
-        "watched": _trakt_request(f"users/{username}/watched/movies")
-        + _trakt_request(f"users/{username}/watched/shows"),
-        "collection": _trakt_request(f"users/{username}/collection/movies")
-        + _trakt_request(f"users/{username}/collection/shows"),
-        "stats": _trakt_request(f"users/{username}/stats"),
-        "history": list(_trakt_paginate(f"users/{username}/history")),
+        "followers": _trakt_request(f"users/{username}/followers", logger=logger),
+        "following": _trakt_request(f"users/{username}/following", logger=logger),
+        "settings": _trakt_request("users/settings", logger=logger),
+        "likes": _trakt_request("users/likes", logger=logger),
+        "profile": _trakt_request(f"users/{username}", logger=logger),
+        "comments": _trakt_request(f"users/{username}/comments", logger=logger),
+        "lists": _trakt_request(f"users/{username}/lists", logger=logger),
+        "ratings": _trakt_request(f"users/{username}/ratings", logger=logger),
+        "recommendations": _trakt_request(
+            f"users/{username}/recommendations", logger=logger
+        ),
+        "watchlist": _trakt_request(f"users/{username}/watchlist", logger=logger),
+        "watched": _trakt_request(f"users/{username}/watched/movies", logger=logger)
+        + _trakt_request(f"users/{username}/watched/shows", logger=logger),
+        "collection": _trakt_request(
+            f"users/{username}/collection/movies", logger=logger
+        )
+        + _trakt_request(f"users/{username}/collection/shows", logger=logger),
+        "stats": _trakt_request(f"users/{username}/stats", logger=logger),
+        "history": list(_trakt_paginate(f"users/{username}/history", logger=logger)),
     }
 
 
